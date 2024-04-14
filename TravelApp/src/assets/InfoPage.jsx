442ab9eda4d85626d984939f0d7be9a4fe8b7f09
Ishani Das <ishanidas05@gmail.com>
2024-04-14 09:34:55 -0400

    morre forntend stuff

---
+++ TravelApp/src/assets/InfoPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function InfoPage() {
+  return (
+    <div>
+      <h2>Info Page</h2>
+      <p>This is the information page.</p>
+      <p>City: </p>
+      <p>Description: </p>
+    </div>
+  );
+}
+
+export default InfoPage;
